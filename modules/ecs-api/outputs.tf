37a41ac7de5ca3b787bc2ef93f54073d376a2cf8
ElizabethHuang <lizne6z0@gmail.com>
2026-09-19 21:48:16 +0800

    feat: Output ALB DNS name.

--- modules/ecs-api/outputs.tf
+++ modules/ecs-api/outputs.tf
@@ -2,6 +2,10 @@ output "alb_arn_suffix" {
   value = aws_lb.api.arn_suffix
 }
 
+output "alb_dns_name" {
+  value = aws_lb.api.dns_name
+}
+
 output "target_group_arn_suffix" {
   value = aws_lb_target_group.api.arn_suffix
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "bastion_public_ip" {
   value = module.vpc.bastion_public_ip
 }
+
+output "alb_dns_name" {
+  value = module.ecs-api.alb_dns_name
+}
